ee9a65d09367305e19e50c8974c25c1598d6551e
Obinna Nneji <nobinnavincent@gmail.com>
2022-06-02 14:53:28 +0100

    Day 2 completed

--- Day 2/find_the_mode.py
+++ Day 2/find_the_mode.py
@@ -1,7 +1,15 @@
-# List to Set
-# Create a function that takes in a list as input, and returns(and prints) a new list
-# with all repetitions reduced to one appearance alone, as output.
-# E.G: f(["a", "b", "a", "a", 3, 3, 2, "hello", "b"] ) => ["a", "b", 3, 2, "hello"]. #output
+# Find the mode
+# Extend the function from day 1, to also print out the modal element(s) of the input list.
+# i.e, to determine which element occurs the most. If there are multiple such elements,
+# then return a list containing them all.
+
+from statistics import multimode
+""" The multimode() function in the statistics module used in line 18 
+    takes a data set as a parameter and returns a list of modes. 
+    We can use this function when there is more than one possible 
+    modal value in a given record.
+"""
+
 
 input_list = input("Please enter a list of items, separate them by a comma(,): ")
 # the input takes a list of items separated by a comma(,).
@@ -16,9 +24,11 @@ def list_to_set(n):
     new_set(Set): A set; sets the items in item_list to a set, removing the duplicates.
     return list(new_set): Returns a list of items in new_set.
     """
+    mode_list = multimode(n)
+    print("The modal value(s) of the list is/are %s" % mode_list)
     new_set = set(n)
     return list(new_set)
 
 
 returned_list = list_to_set(item_list)
-print(returned_list)
+print("The new list without duplicate is {}".format(returned_list))
